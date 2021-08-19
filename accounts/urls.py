edad28e816d8f6327bd5404618b152e648f1962f
Rafael Lima <47rafael.lima@gmail.com>
2021-08-19 17:02:13 -0300

    urls.py update

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -10,10 +9,10 @@ urlpatterns = [
 
 # URL's to be implemented
 '''
-path("password_change/", auth_views.PasswordChangeView.as_view(), name='password_change'),
-path("password_change/done/", auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
-path("password_reset/", auth_views.PasswordResetView.as_view(), name='password_reset'),
-path("password_reset/done/", auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-path("reset/done/", auth_views.PasswordResetDoneView.as_view(), name='password_reset_complete'),
+path("password_change/", views.PasswordChangeView.as_view(), name='password_change'),
+path("password_change/done/", views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+path("password_reset/", views.PasswordResetView.as_view(), name='password_reset'),
+path("password_reset/done/", views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+path("reset/<uidb64>/<token>/", views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+path("reset/done/", views.PasswordResetDoneView.as_view(), name='password_reset_complete'),
 '''
