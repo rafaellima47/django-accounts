412a9b4eb9a2383626d6b7d2447f39f371d72365
Rafael Lima <47rafael.lima@gmail.com>
2021-08-18 23:44:18 -0300

    Security update

--- django_accounts/settings.py
+++ django_accounts/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tm!gn##%jhy@0e_qi5s!&1rp8gnt%620pr*f5p^8)-z_*k&b*e'
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
